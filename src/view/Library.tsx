7c3624bca83eb378996b7380abbf94f0510ac4d7
thineth <tparcg@gmail.com>
2022-05-22 02:12:51 +0530

    Installed Bootstrap, scss, Raleway font and Created Welcome UX

---
+++ src/view/Library.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Col, Container, Row} from "react-bootstrap";
+import Welcome from "../components/Welcome";
+
+const Library: React.FC = () => {
+    return (
+        <Container fluid={true}>
+            <Row>
+                <Col xs={12}>
+                    <Welcome/>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Library;
